2eb068d6cd1ffd9d8d8965646a0a7e757ebf43e6
SpicyChickenFLY <xixiayuchao@gmail.com>
2021-03-09 09:24:39 +0800

    create frontend framework

--- main.go
+++ main.go
@@ -1,27 +1,4 @@
 package main
 
-import (
-	"fmt"
-)
-
-// PlaygroundBackend - Playground structs implement this interface can be
-// deployed by our engine
-type PlaygroundBackend interface {
-	InitNewGame() error
-	GetGameData() (data string, err error)
-	ExecuteCmd(command string) error
-	SaveGame(filePath string) error
-	LoadGame(filePath string) error
-}
-
 func main() {
-	// NewPlayground()
-	playground := NewPlayground(gameModeStandard)
-	playground.InitNewGame()
-	fmt.Println(playground.GetGameData())
-	// playground.Move
-	cmdStr := `{
-
-	}`
-	playground.ExecuteCmd(cmdStr)
 }
